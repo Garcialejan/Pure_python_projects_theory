03e0ba67449b31a0f0d4d51044b1e76e817651fe
Alejandro García <garcialejan@gmail.com>
2024-05-17 18:14:47 +0200

    Reorganized Python folder

--- Iniciacion/main.py
+++
@@ -1,9 +0,0 @@
-tempF = int(input("¿Qué temperatura hace hoy en ºF: "))
-tempC = (tempF-32)*(5/9)
-print("La temperatura en grados es: {}".format(tempC))
-
-
-fcambio = float(input("¿A cuánto está el cambio de libras a euros? "))
-libra = int(input("Cantidad de libras: "))
-euro = libra * fcambio
-print("La cantidad en euros es: {}".format(euro))
