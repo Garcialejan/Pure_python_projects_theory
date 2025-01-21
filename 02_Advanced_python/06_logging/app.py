75a11fe14ab1478ca0774b9f85e60204da7263de
Alejandro Garcia <garcialejan@gmail.com>
2025-01-21 18:56:38 +0100

    Logging exercises done

--- 02_Advanced_python/06_logging/app.py
+++ 02_Advanced_python/06_logging/app.py
@@ -1,44 +1,43 @@
 import logging
 
 ## logging setting
-
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     handlers=[
-        logging.FileHandler("app1.log"),
-        logging.StreamHandler()
+        logging.FileHandler("app1.log"), # To define the name where logs are saved
+        logging.StreamHandler() # To sure the logs are been writed in the log file
     ]
 )
 
 logger=logging.getLogger("ArithmethicApp")
 
-def add(a,b):
-    result=a+b
-    logger.debug(f"Adding {a} + {b}= {result}")
+def add(a, b):
+    result = a + b
+    logger.debug(f"Adding {a} + {b} = {result}")
     return result
 
-def subtract(a, b):
+def substract(a, b):
     result = a - b
-    logger.debug(f"Subtracting {a} - {b} = {result}")
-    return result
+    logger.debug(f"Substract {a} - {b} = {result}")
+    return result 
 
 def multiply(a, b):
     result = a * b
-    logger.debug(f"Multiplying {a} * {b} = {result}")
-    return result
+    logger.debug(f"Multipliying {a} * {b} = {result}")
+    return result 
 
 def divide(a, b):
     try:
         result = a / b
-        logger.debug(f"Dividing {a} / {b} = {result}")
+        logger.debug(f"Substract {a} / {b} = {result}")
         return result
-    except ZeroDivisionError:
+    except ZeroDivisionError as e:
         logger.error("Division by zero error")
         return None
     
 add(10,15)
-subtract(15,10)
+substract(15,10)
 multiply(10,20)
-divide(20,0)
+divide(20,5)
